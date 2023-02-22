b9d0aeb18a9d0c631152ae483cabd45e4b255680
Alian Peralta <Alian Peralta@DESKTOP-1AKI9U0>
2023-02-22 09:32:54 -0400

    aa

--- src/DF_presentacion/frm_Login.java
+++ src/DF_presentacion/frm_Login.java
@@ -26,6 +26,7 @@ public class frm_Login extends javax.swing.JFrame {
     }
    public boolean RevisarUsuario(String usuario){
         //Funcion para Revisar si el usuario existe dentro de la BD
+        String a;
         PreparedStatement ps;
         ResultSet rs;
         boolean checkUser = false;
